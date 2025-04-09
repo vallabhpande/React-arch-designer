3ce9ae65ad78afd7924442ed3fc0030f83550ec6
Vallabh Pande <vallabhpande79@gmail.com>
2025-04-09 22:00:23 +0530

    UI changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,19 +10,23 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="logo">Shreejay Architect And Associates</div>
-      <ul className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
-        <li><a href="#hero">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-      <div className="hamburger" onClick={toggleMenu}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
+      <div className="navbar-container">
+        <div className="logo">Shreejay Architect & Associates</div>
+
+        <ul className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className={`hamburger ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
       </div>
     </nav>
   );
